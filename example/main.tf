dceae350098dfa35eeaf50c02c8c2eed82b1ada1
Alex Bance <Alex.Bance@justice.gov.uk>
2024-08-28 09:43:21 +0100

    fix: remove deprecated subnet property

--- example/main.tf
+++ example/main.tf
@@ -32,7 +32,6 @@ resource "azurerm_subnet" "test" {
   resource_group_name                           = azurerm_resource_group.test.name
   virtual_network_name                          = azurerm_virtual_network.test.name
   address_prefixes                              = ["10.0.1.0/24"]
-  enforce_private_link_service_network_policies = true
 }
 
 resource "azurerm_lb" "test" {
