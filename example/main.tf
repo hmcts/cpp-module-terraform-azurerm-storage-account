16fe25afbf251891f733f05b58b621690c434241
Alex Bance <Alex.Bance@justice.gov.uk>
2024-08-29 09:26:01 +0100

    fix: added private_endpoint_network_polices

--- example/main.tf
+++ example/main.tf
@@ -33,6 +33,7 @@ resource "azurerm_subnet" "test" {
   virtual_network_name                          = azurerm_virtual_network.test.name
   address_prefixes                              = ["10.0.1.0/24"]
   private_link_service_network_policies_enabled = true
+  private_endpoint_network_policies             = "Enabled"
 }
 
 resource "azurerm_lb" "test" {
