c1baed82187ed588d00aa5dd77b7c106f91b6a3a
Simran Sian <simran.sian1@justice.gov.uk>
2024-08-30 09:16:15 +0100

    Switch string to bool

--- example/main.tf
+++ example/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_subnet" "test" {
   resource_group_name                           = azurerm_resource_group.test.name
   virtual_network_name                          = azurerm_virtual_network.test.name
   address_prefixes                              = ["10.0.1.0/24"]
-  private_link_service_network_policies_enabled = "Disabled"
+  private_link_service_network_policies_enabled = false
 }
 
 resource "azurerm_lb" "test" {
