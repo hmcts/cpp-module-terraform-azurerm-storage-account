621959ed7748f6b9a845f647435392c7ac571348
Simran Sian <simran.sian1@justice.gov.uk>
2024-08-30 09:13:08 +0100

    Format

--- example/main.tf
+++ example/main.tf
@@ -28,10 +28,10 @@ resource "azurerm_virtual_network" "test" {
 }
 
 resource "azurerm_subnet" "test" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.test.name
-  virtual_network_name = azurerm_virtual_network.test.name
-  address_prefixes     = ["10.0.1.0/24"]
+  name                                          = "example-subnet"
+  resource_group_name                           = azurerm_resource_group.test.name
+  virtual_network_name                          = azurerm_virtual_network.test.name
+  address_prefixes                              = ["10.0.1.0/24"]
   private_link_service_network_policies_enabled = "Disabled"
 }
 
