aa1ac7802074c8fa466e9c4b5cf9f078b3286f7a
santhosh <santhosh.kotha@hmcts.net>
2026-06-05 14:32:53 +0100

    Support configuring life cycle policy extra rules with optional creation_day config (#24)

--- main.tf
+++ main.tf
@@ -76,8 +76,11 @@ resource "azurerm_storage_management_policy" "main_extra" {
           delete_after_days_since_modification_greater_than = rule.value.days
           delete_after_days_since_creation_greater_than     = rule.value.creation_day
         }
-        snapshot {
-          delete_after_days_since_creation_greater_than = rule.value.creation_day
+        dynamic "snapshot" {
+          for_each = rule.value.creation_day != null ? [1] : []
+          content {
+            delete_after_days_since_creation_greater_than = rule.value.creation_day
+          }
         }
       }
     }
