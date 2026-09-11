284667069e62e621c587880f06a6ffcbc690a657
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-09-11 13:56:05 +0100

    update references

--- main.tf
+++ main.tf
@@ -211,7 +211,7 @@ resource "azurerm_private_endpoint" "endpoint_table" {
 resource "azurerm_storage_container" "container" {
   count                 = var.containers_list == null ? 0 : length(var.containers_list)
   name                  = var.containers_list[count.index].name
-  storage_account_name  = azurerm_storage_account.main.name
+  storage_account_id    = azurerm_storage_account.main.id
   container_access_type = var.containers_list[count.index].access_type
   depends_on = [
     azurerm_private_endpoint.endpoint_blob,
